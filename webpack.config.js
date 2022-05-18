880669cf11606254fd06cabb53bfde67a7c91258
Jessica Yu <yulejessica@gmail.com>
2022-05-18 14:24:14 -0700

    webpack is working

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,39 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+var SRC_DIR = path.join(__dirname, "/client/src");
 
 module.exports = {
   mode: 'development',
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: `${SRC_DIR}/index.js`,
   output: {
-    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    path: path.join(__dirname, "/client/dist"),
   },
   module: {
     rules: [
       {
-        test: /\.(jsx|js)$/,
+        test: /\.(jsx|js)?/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            // plugins: [
+            //   new HtmlWebpackPlugin({
+            //     template: path.join(__dirname, "/client/src", "index.html"),
+            //   }),
+            // ],
+            plugins: [
+              ["@babel/plugin-transform-runtime",
+                {
+                  "regenerator": true
+                }
+              ]
+            ]
           }
         }
       },
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
-    }),
-  ],
-}
+
+};
